56c8930496140ef5802155a57b1079de76854850
vijay <vijaya.kanugovi@cgi.com>
2019-08-01 10:22:25 +0530

    user service project of  spring boot

---
+++ src/main/java/com/stackroute/userservice/UserserviceApplication.java
@@ -0,0 +1,19 @@
+package com.stackroute.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+//@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class UserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserserviceApplication.class, args);
+	}
+
+}
